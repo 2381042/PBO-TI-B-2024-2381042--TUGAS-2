b56af94b82d2044c5f2ff98a2ed64d5ef22ecb21
2381042 <2381042@unai.edu>
2024-09-25 21:01:46 +0700

    Update Tugas2.java

--- Tugas2.java
+++ Tugas2.java
@@ -5,27 +5,22 @@ public class Tugas2 {
     public static double luasPersegi(double sisi) {
         return sisi * sisi;
     }
-
     public static double kelilingPersegi(double sisi) {
         return 4 * sisi;
     }
-
     public static double luasPersegiPanjang(double panjang, double lebar) {
         return panjang * lebar;
     }
-
     public static double kelilingPersegiPanjang(double panjang, double lebar) {
         return 2 * (panjang + lebar);
     }
-
     public static double luasLingkaran(double jariJari) {
         return Math.PI * jariJari * jariJari;
     }
-
     public static double kelilingLingkaran(double jariJari) {
         return 2 * Math.PI * jariJari;
     }
-
+    
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Pilih bangun datar: ");
@@ -42,7 +37,6 @@ public class Tugas2 {
                 System.out.println("Luas Persegi: " + luasPersegi(sisi));
                 System.out.println("Keliling Persegi: " + kelilingPersegi(sisi));
                 break;
-
             case 2:
                 System.out.println("Masukkan panjang persegi panjang: ");
                 double panjang = input.nextDouble();
@@ -50,20 +44,14 @@ public class Tugas2 {
                 double lebar = input.nextDouble();
                 System.out.println("Luas persegi panjang: " + kelilingPersegiPanjang(panjang, lebar));
                 break;
-
             case 3:
                 System.out.println("Masukkan jari-jari lingkaran: ");
                 double jariJari = input.nextDouble();
                 System.out.println("Luas lingkaran: " + kelilingLingkaran(jariJari));
                 break;
-
             default:
                 System.out.println("Pilihan tidak valid");
-
         }
         input.close();
     }
 }
-
-
-
